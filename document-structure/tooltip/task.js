9787cdb5133ec66a79a2be24eccf856be774a776
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-05-27 09:40:18 +0300

    first commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -6,27 +6,40 @@ for (let i = 0; i < arrayTooltip.length; i++) {
     let active = false;
     let textTooltip = arrayTooltip[i].getAttribute('title');
     newDiv[i] = document.createElement('div');
-    newDiv[i].setAttribute('class', 'tooltip');
+    newDiv[i].classList.add('tooltip');
     newDiv[i].innerHTML = `${textTooltip}`;
 
-    arrayTooltip[i].addEventListener('click', function(event) {
+
+    arrayTooltip[i].onclick = function(event) {
         event.preventDefault();
 
         if (i !== activeTooltip) {
-            newDiv[activeTooltip].classList.remove('tooltip_active');
-            arrayTooltip[activeTooltip].appendChild(newDiv[activeTooltip]);
+            newDiv[activeTooltip].remove();
             active = false;
         }
 
         if (!active) {
             newDiv[i].classList.add('tooltip_active');
-            arrayTooltip[i].appendChild(newDiv[i]);            
+            arrayTooltip[i].append(newDiv[i]);
+            let coords = arrayTooltip[i].getBoundingClientRect();
+
+            let left = coords.left + (arrayTooltip[i].offsetWidth - newDiv[i].offsetWidth) / 2;
+            if (left < 0) left = 0; // не заезжать за левый край окна
+
+            let top = coords.top - newDiv[i].offsetHeight - 5;
+            if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
+                top = coords.top + arrayTooltip[i].offsetHeight + 5;
+            }
+
+            newDiv[i].style.left = left + 'px';
+            newDiv[i].style.top = top + 'px';
+
             active = true;
             activeTooltip = i;
         } else {
-            newDiv[i].classList.remove('tooltip_active');
-            arrayTooltip[i].appendChild(newDiv[i]); 
+            newDiv[i].remove();
             active = false;
         }
-    })
+
+    }
 }
